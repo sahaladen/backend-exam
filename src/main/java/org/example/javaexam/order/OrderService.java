9ee42cff5be5abd6d9d19710676096708a4c2d53
sahal aden <saad012@student.kristiania.no>
2024-12-03 22:14:19 +0100

    la til mer kode til alle service

--- src/main/java/org/example/javaexam/order/OrderService.java
+++ src/main/java/org/example/javaexam/order/OrderService.java
@@ -1,7 +1,37 @@
 package org.example.javaexam.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderService {
+    private final OrderRepo orderRepo;
+
+    @Autowired
+    public OrderService(OrderRepo orderRepo) {
+        this.orderRepo = orderRepo;
+    }
+
+    public List<Order> getOrder() {
+        return orderRepo.findAll();
+    }
+
+    public Order getOrderById(long id){
+        return orderRepo.findById(id).orElse(null);
+    }
+
+    public Order saveOrder(Order order) {
+        return orderRepo.save(order);
+    }
+
+    public void deleteOrder(){
+        orderRepo.deleteAll();
+    }
+
+    public void deleteOrderById(long id){
+        orderRepo.deleteById(id);
+    }
+
 }
