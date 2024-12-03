5d7997d5b9a15e738591e9221307a96790ce31f8
sahal aden <saad012@student.kristiania.no>
2024-12-03 22:20:13 +0100

    la til mer kode

--- src/main/java/org/example/javaexam/customer/Customer.java
+++ src/main/java/org/example/javaexam/customer/Customer.java
@@ -7,8 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
+@Getter @Setter
 @NoArgsConstructor
 public class Customer {
 
--- src/main/java/org/example/javaexam/order/Order.java
+++ src/main/java/org/example/javaexam/order/Order.java
@@ -1,4 +1,20 @@
 package org.example.javaexam.order;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.javaexam.product.Product;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Order {
+
+    @Id
+    private long orderId;
 }
