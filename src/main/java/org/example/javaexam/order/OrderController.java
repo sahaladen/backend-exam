e4ec05078382fb14b0f5c3a991f17a64442ca0fe
sahal aden <saad012@student.kristiania.no>
2024-12-03 20:15:29 +0100

    la til controller det  jeg mangla

---
+++ src/main/java/org/example/javaexam/order/OrderController.java
@@ -0,0 +1,9 @@
+package org.example.javaexam.order;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+}
