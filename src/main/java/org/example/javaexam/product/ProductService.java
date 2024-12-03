48b1a43436d1ab6981d61d3fe91819a898857d90
sahal aden <saad012@student.kristiania.no>
2024-12-03 20:02:21 +0100

    fikset application.yml.

--- src/main/java/org/example/javaexam/product/ProductService.java
+++ src/main/java/org/example/javaexam/product/ProductService.java
@@ -1,7 +1,35 @@
 package org.example.javaexam.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
+    private final ProductRepo productRepo;
+
+    @Autowired
+    public ProductService(ProductRepo productRepo) {
+        this.productRepo = productRepo;
+    }
+
+    public List<Product> getProduct() {
+        return productRepo.findAll();
+    }
+
+    public Product getProductById(long id) {
+        return productRepo.findById(id).orElse(null);
+    }
+
+    public Product saveProduct(Product product){
+        return productRepo.save(product);
+    }
+
+    public void deleteProduct() {
+        productRepo.deleteAll();
+    }
+    public void deleteProductById(long id) {
+        productRepo.deleteById(id);
+    }
 }
