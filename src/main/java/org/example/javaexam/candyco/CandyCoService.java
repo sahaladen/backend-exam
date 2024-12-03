77dc4660a03420f6da51dc25d988cc1258565daf
sahal aden <saad012@student.kristiania.no>
2024-12-03 22:23:44 +0100

    la til alle service i candyco service

--- src/main/java/org/example/javaexam/candyco/CandyCoService.java
+++ src/main/java/org/example/javaexam/candyco/CandyCoService.java
@@ -1,7 +1,23 @@
 package org.example.javaexam.candyco;
 
+import org.example.javaexam.customer.CustomerService;
+import org.example.javaexam.order.OrderService;
+import org.example.javaexam.product.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CandyCoService {
+    private CustomerService customerService;
+    private OrderService orderService;
+    private ProductService productService;
+    private CandCoRepo candCoRepo;
+
+    @Autowired
+    public CandyCoService(CustomerService customerService, OrderService orderService, ProductService productService, CandCoRepo candCoRepo) {
+        this.customerService = customerService;
+        this.orderService = orderService;
+        this.productService = productService;
+        this.candCoRepo = candCoRepo;
+    }
 }
